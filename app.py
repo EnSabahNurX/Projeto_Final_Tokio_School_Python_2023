b2f848f6b0bd0bf1140e813daf5bf8eabc42f847
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-07-23 13:08:14 +0100

    implementado o conceito de migrações na base de dados

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime, timedelta
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import pytz
 
 app = Flask(__name__)
@@ -20,48 +21,57 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db = SQLAlchemy(app)
 
-# Definição dos modelos de dados
+# Configuração do Flask-Migrate
+migrate = Migrate(app, db)
+
+# Definição das classes Carro, Mota e Disponibilidade
+
+
 class Carro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(50))
-    modelo = db.Column(db.String(50))
-    ano = db.Column(db.Integer)
-    diaria = db.Column(db.Float)
-    combustivel = db.Column(db.String(20))
-    lugares = db.Column(db.Integer)
-    potencia = db.Column(db.Integer)
-    transmissao = db.Column(db.String(50))
-    categoria = db.Column(db.String(50))
+    marca = db.Column(db.String(100), nullable=False)
+    modelo = db.Column(db.String(100), nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
+    diaria = db.Column(db.Float, nullable=False)
+    combustivel = db.Column(db.String(50), nullable=False)
+    lugares = db.Column(db.Integer, nullable=False)
+    potencia = db.Column(db.String(50), nullable=False)
+    transmissao = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(50), nullable=False)
     utilizacoes = db.Column(db.Integer, default=0)
-    limite_utilizacoes = db.Column(db.Integer)
-    data_legalizacao = db.Column(db.Date)
-    data_alerta_legalizacao = db.Column(db.Date)
-    disponibilidades = db.relationship('Disponibilidade', backref='carro', lazy=True)
+    limite_utilizacoes = db.Column(db.Integer, nullable=False, default=5)
+    data_legalizacao = db.Column(db.Date, nullable=False)
+    data_alerta_legalizacao = db.Column(db.Date, nullable=False)
+
 
 class Mota(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    marca = db.Column(db.String(50))
-    modelo = db.Column(db.String(50))
-    ano = db.Column(db.Integer)
-    diaria = db.Column(db.Float)
-    combustivel = db.Column(db.String(20))
-    lugares = db.Column(db.Integer)
-    cilindradas = db.Column(db.Integer)
-    peso = db.Column(db.Float)
-    categoria = db.Column(db.String(50))
+    marca = db.Column(db.String(100), nullable=False)
+    modelo = db.Column(db.String(100), nullable=False)
+    ano = db.Column(db.Integer, nullable=False)
+    diaria = db.Column(db.Float, nullable=False)
+    cilindradas = db.Column(db.String(50), nullable=False)
+    peso = db.Column(db.String(50), nullable=False)
+    categoria = db.Column(db.String(50), nullable=False)
     utilizacoes = db.Column(db.Integer, default=0)
-    limite_utilizacoes = db.Column(db.Integer)
-    data_legalizacao = db.Column(db.Date)
-    data_alerta_legalizacao = db.Column(db.Date)
-    disponibilidades = db.relationship('Disponibilidade', backref='mota', lazy=True)
+    limite_utilizacoes = db.Column(db.Integer, nullable=False, default=3)
+    data_legalizacao = db.Column(db.Date, nullable=False)
+    data_alerta_legalizacao = db.Column(db.Date, nullable=False)
+
 
 class Disponibilidade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.Date)
-    carro_id = db.Column(db.Integer, db.ForeignKey('carro.id'))
-    mota_id = db.Column(db.Integer, db.ForeignKey('mota.id'))
+    carro_id = db.Column(db.Integer, db.ForeignKey('carro.id'), nullable=True)
+    carro = db.relationship('Carro', backref=db.backref(
+        'disponibilidades', lazy=True))
+    mota_id = db.Column(db.Integer, db.ForeignKey('mota.id'), nullable=True)
+    mota = db.relationship('Mota', backref=db.backref(
+        'disponibilidades', lazy=True))
+    data_disponivel = db.Column(db.Date, nullable=False)
 
 # Event Listener para calcular a data de alerta antes de salvar o registro
+
+
 @db.event.listens_for(Carro, 'before_insert')
 @db.event.listens_for(Carro, 'before_update')
 @db.event.listens_for(Mota, 'before_insert')
@@ -70,42 +80,47 @@ def calculate_alert_date(mapper, connection, target):
     if target.data_legalizacao:
         # Obtém o fuso horário de Portugal
         tz = pytz.timezone(app.config['TIMEZONE'])
-        
+
         # Converte a data de legalização para o fuso horário de Portugal
         data_legalizacao_portugal = tz.localize(target.data_legalizacao)
-        
+
         # Calcula a data de alerta para legalização (30 dias antes)
-        data_alerta_legalizacao = data_legalizacao_portugal - timedelta(days=30)
-        
+        data_alerta_legalizacao = data_legalizacao_portugal - \
+            timedelta(days=30)
+
         # Converte a data de alerta de volta para o formato local de Portugal
         target.data_alerta_legalizacao = data_alerta_legalizacao.date()
-
 
 
 # Criação das tabelas do banco de dados
 if not os.path.exists('database.db'):
     with app.app_context():
         db.create_all()
-        #db.session.commit()
+        # db.session.commit()
 else:
     with app.app_context():
         db.session.commit()
 
+
 # Definição das rotas
+
+
 @app.route('/')
 def home():
     return 'Luxury Wheels, aluguer de carros e motas'
+
 
 @app.route('/carros')
 def listar_carros():
     return 'Listagem de carros'
 
+
 @app.route('/carros/<int:carro_id>')
 def exibir_carro(carro_id):
     return f'Detalhes do carro {carro_id}'
+
 
 # Execução da app
 if __name__ == '__main__':
     # O debug=True faz com que cada vez que reiniciemos o servidor ou modifiquemos o código, o servidor de Flask reinicia-se sozinho
     app.run(debug=True)
-
