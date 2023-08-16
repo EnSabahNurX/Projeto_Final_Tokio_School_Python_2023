bdd5544c386f300e226482a17041341ec6dae459
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-08-16 19:22:54 +0100

    update

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ricardo/VSCode/projeto_final/.venv"
+set -gx VIRTUAL_ENV "/media/ricardo/Arch/home/ricardo/VSCode/projeto_final/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
