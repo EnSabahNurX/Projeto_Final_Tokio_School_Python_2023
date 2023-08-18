28654142b50b391e8c31eb978dcb8d66a0d11fdc
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-08-19 00:22:30 +0100

    limitando acesso a página de reserva a clientes logados

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from flask import session, redirect, url_for
+from flask import session, redirect, url_for, request
+
 
 def login_required(f):
     @wraps(f)
@@ -8,4 +9,5 @@ def login_required(f):
             # Se o usuário não estiver autenticado, redirecione para a página de login
             return redirect(url_for("login", next=request.url))
         return f(*args, **kwargs)
+
     return decorated_function
