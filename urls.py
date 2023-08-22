d0b907052cd70373a905aab5093c440662031165
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-08-22 22:10:37 +0100

    reestruturando o projeto

---
+++ urls.py
@@ -0,0 +1,62 @@
+from app import app
+from views import *
+
+# Rota inicial
+app.add_url_rule("/", view_func=index)
+
+# Rota para a página de detalhes do veículo
+app.add_url_rule("/vehicle/<int:id>", view_func=vehicle_details)
+
+# Rota para a página de reserva do veículo
+app.add_url_rule("/reserve/<int:id>", view_func=reserve)
+
+# Rota para processar o pagamento
+app.add_url_rule("/complete_payment", view_func=complete_payment)
+
+# Rota para a página de confirmação de pagamento
+app.add_url_rule("/order_confirmation", view_func=order_confirmation)
+
+
+# Rota para a página de login do administrador
+app.add_url_rule("/login", view_func=login)
+
+# Rota para o painel de administração
+app.add_url_rule("/admin", view_func=admin_panel)
+
+# Rota para visualisar os detalhes do veículo pelo admin
+app.add_url_rule("/admin/view_vehicle/<int:id>",view_func=view_vehicle)
+
+# Rota para a página de adicionar veículos
+app.add_url_rule("/add_vehicle", view_func=add_vehicle)
+
+# Rota para a página de edição de veículo
+app.add_url_rule("/edit_vehicle/<int:id>", view_func=edit_vehicle)
+
+# Rota para exclusão de veículo
+app.add_url_rule("/delete_vehicle/<int:id>", view_func=delete_vehicle)
+
+# Rota para exclusão de imagem
+app.add_url_rule("/delete_image/<path:image_path>/<int:vehicle_id>", view_func=delete_image
+)
+
+# Rota para a página de login do cliente
+app.add_url_rule("/client_login", view_func=client_login)
+
+# Rota para logout do cliente
+app.add_url_rule("/client_logout"view_func=client_logout)
+
+# Rota para logout do admin
+app.add_url_rule("/logout"view_func=logout)
+
+# Rota para a página de registro do cliente
+app.add_url_rule("/register_client", view_func=register_client)
+
+# Rota para confirmar a legalização do veículo
+app.add_url_rule("/legalize_vehicle/<int:id>", view_func=legalize_vehicle)
+
+# Rota para manutenção do veículo
+app.add_url_rule("/admin/maintenance_vehicle/<int:id>", view_func=
+    maintenance_vehicle
+)
+# Rota para atualizar o número de utilização do veículo
+app.add_url_rule("/register_usage/<int:vehicle_id>", view_func=register_usage_route)
--- views.py
+++ views.py
@@ -1,92 +1,27 @@
-from flask import Flask
-from app import app
-from controllers import (
-    index,
-    vehicle_details,
-    reserve,
-    complete_payment,
-    order_confirmation,
-    admin_panel,
-    view_vehicle,
-    add_vehicle,
-    edit_vehicle,
-    delete_vehicle,
-    delete_image,
-    client_login,
-    client_logout,
-    logout,
-    register_client,
-    legalize_vehicle,
-    maintenance_vehicle,
-    register_usage_route,
-)
+# views.py
 
+from flask import render_template, request, redirect, url_for, session, flash
+from models import db, Vehicle, VehicleType, Cliente
+from config import Config
+from decorators import login_required
 
-# Cria um Blueprint para as rotas
-app_views = Blueprint("app_views", __name__)
+# Importe qualquer função auxiliar necessária
 
+# Defina as funções de visualização
 
-# Rotas
+def index():
+    # Sua implementação para a rota inicial
 
-# Rota inicial
-app.route("/")(index)
+def vehicle_details(id):
+    # Sua implementação para a página de detalhes do veículo
 
-# Rota para a página de detalhes do veículo
-app.route("/vehicle/<int:id>")(vehicle_details)
+def reserve(id):
+    # Sua implementação para a página de reserva do veículo
 
-# Rota para a página de reserva do veículo
-app.route("/reserve/<int:id>", methods=["GET", "POST"])(reserve)
+def complete_payment():
+    # Sua implementação para processar o pagamento
 
-# Rota para processar o pagamento
-app.route("/complete_payment", methods=["POST"])(complete_payment)
+def order_confirmation():
+    # Sua implementação para a página de confirmação de pagamento
 
-# Rota para a página de confirmação de pagamento
-app.route("/order_confirmation")(order_confirmation)
-
-# Função de middleware para verificar a sessão de administrador
-app.before_request(check_admin_session)
-
-# Rota para a página de login do administrador
-app.route("/login", methods=["GET", "POST"])(login)
-
-# Rota para o painel de administração
-app.route("/admin", methods=["GET", "POST"])(admin_panel)
-
-# Rota para visualisar os detalhes do veículo pelo admin
-app.route("/admin/view_vehicle/<int:id>")(view_vehicle)
-
-# Rota para a página de adicionar veículos
-app.route("/add_vehicle", methods=["GET", "POST"])(add_vehicle)
-
-# Rota para a página de edição de veículo
-app.route("/edit_vehicle/<int:id>", methods=["GET", "POST"])(edit_vehicle)
-
-# Rota para exclusão de veículo
-app.route("/delete_vehicle/<int:id>", methods=["POST"])(delete_vehicle)
-
-# Rota para exclusão de imagem
-app.route("/delete_image/<path:image_path>/<int:vehicle_id>", methods=["POST"])(
-    delete_image
-)
-
-# Rota para a página de login do cliente
-app.route("/client_login", methods=["GET", "POST"])(client_login)
-
-# Rota para logout do cliente
-app.route("/client_logout")(client_logout)
-
-# Rota para logout do admin
-app.route("/logout")(logout)
-
-# Rota para a página de registro do cliente
-app.route("/register_client", methods=["GET", "POST"])(register_client)
-
-# Rota para confirmar a legalização do veículo
-app.route("/legalize_vehicle/<int:id>", methods=["POST"])(legalize_vehicle)
-
-# Rota para manutenção do veículo
-app.route("/admin/maintenance_vehicle/<int:id>", methods=["GET", "POST"])(
-    maintenance_vehicle
-)
-# Rota para atualizar o número de utilização do veículo
-app.route("/register_usage/<int:vehicle_id>", methods=["POST"])(register_usage_route)
+# ... defina mais funções de visualização conforme necessário ...
