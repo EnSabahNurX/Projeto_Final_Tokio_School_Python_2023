1cab1d86b64c3df8401e889e7b596b8e73deb46a
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-09-02 16:04:11 +0100

    implementação da classe Categoria

--- models.py
+++ models.py
@@ -186,6 +186,8 @@ class Reservation(db.Model):
 
 
 class Categoria(db.Model):
+    __tablename__ = "categorias"
+
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(255), unique=True, nullable=False)
 
