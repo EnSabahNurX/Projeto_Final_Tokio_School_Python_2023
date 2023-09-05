fc4e1168bd06b70681e836b73e307895f2e52e80
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-09-05 17:43:09 +0100

    melhoramento em comentários, docstrings, limpeza e otimização de código

--- config.py
+++ config.py
@@ -1,29 +1,38 @@
 import os
 import pytz
 
-# Caminho do banco de dados
+# Caminhos do banco de dados e imagens estáticas
 db_folder = os.path.join(os.path.dirname(__file__), "database")
 db_path = os.path.join(db_folder, "database.db")
 
-# Verificar e criar o diretório "database" se não existir
+# Verifica e cria o diretório "database" se não existir
 if not os.path.exists(db_folder):
     os.makedirs(db_folder)
 
-# Se a pasta 'static/images' não existir, crie-a
-if not os.path.exists("static/images"):
-    os.makedirs("static/images")
+# Cria o diretório "static/images" se não existir
+static_images_folder = os.path.join(os.path.dirname(__file__), "static/images")
+if not os.path.exists(static_images_folder):
+    os.makedirs(static_images_folder)
 
 
 class Config:
+    """
+    Configurações da aplicação Flask.
+    """
+
     SECRET_KEY = "sua_chave_secreta_aqui"
     TIMEZONE = pytz.timezone("Europe/Lisbon")
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static/images")
+    UPLOAD_FOLDER = static_images_folder
+
+    # Configurações do Bootstrap
     BOOTSTRAP_BOOTSWATCH_THEME = "yeti"
     BOOTSTRAP_USE_MINIFIED = True
     BOOTSTRAP_USE_CDN = True
     BOOTSTRAP_FONTAWESOME = True
+
+    # Cores personalizadas do Bootstrap
     BOOTSTRAP_COLORS = {
         "primary": "#007bff",
         "secondary": "#6c757d",
@@ -33,6 +42,7 @@ class Config:
         "info": "#00bcd4",
     }
 
+    # Ícones do Font Awesome usados no Bootstrap
     BOOTSTRAP_FONTAWESOME_ICONS = [
         "fas fa-car",
         "fas fa-motorcycle",
